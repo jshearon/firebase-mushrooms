1bb813a93c68bec541edb3a11c8c0e42b1e96557
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-06-20 12:00:12 -0500

    added auth component

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,18 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 
-console.error('hi');
+import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+
+import myNavbar from './components/myNavbar/myNavbar';
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
+  auth.logInButton();
+  myNavbar.logoutEvent();
+};
+
+init();
