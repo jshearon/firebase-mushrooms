40b68c5a632a554baf553a6d028a75d9a0302382
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-06-22 19:14:19 -0500

    shroom output

--- src/javascripts/components/mushroomList/mushroomList.js
+++ src/javascripts/components/mushroomList/mushroomList.js
@@ -1,10 +1,24 @@
 import mushroomData from '../../helpers/data/mushroomData';
+import utils from '../../helpers/utils';
 
 // import utils from '../../helpers/utils';
 
 const buildForest = () => {
   mushroomData.getMushrooms()
-    .then((mushrooms) => console.error('Get Mushrooms worked!', mushrooms))
+    .then((mushrooms) => {
+      console.error('Get Mushrooms worked!', mushrooms);
+      let domString = `
+        <h2 class="text-center">Forest</h2>
+        <div class="d-flex flex-wrap justify-center">`;
+
+      mushrooms.forEach((mushroom) => {
+        domString += `<p>${mushroom.name}</p>`;
+      });
+
+      domString += '</div>';
+
+      utils.printToDom('#forest', domString);
+    })
     .catch((err) => console.error('got mushrooms broke', err));
   // const domString = '<h1>It does Work</h1>';
   // utils.printToDom('#forest', domString);
