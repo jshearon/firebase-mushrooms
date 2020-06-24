c8d520c5eb6031d699f8da06826ee44e8a6c1c6e
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-06-23 20:43:32 -0500

    delete mushroom

--- src/javascripts/components/mushroom/mushroom.js
+++ src/javascripts/components/mushroom/mushroom.js
@@ -1,11 +1,12 @@
 const mushroomMaker = (mushroom) => {
   const domString = `
     <div class="col-3">
-    <div class="card">
+    <div class="card" id="${mushroom.id}">
     <div class="card-body">
       <h5 class="card-title">${mushroom.name}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${mushroom.location}</h6>
       <p class="card-text">This mushroom is ${mushroom.size} and ${mushroom.weight}</p>
+      <button class="btn btn-danger delete-shroom"><i class="far fa-trash-alt"></i>Delete Shroom</button>
     </div>
   </div>
   </div>`;
--- src/javascripts/components/mushroomList/mushroomList.js
+++ src/javascripts/components/mushroomList/mushroomList.js
@@ -2,7 +2,16 @@ import mushroomData from '../../helpers/data/mushroomData';
 import utils from '../../helpers/utils';
 import mushroomComponent from '../mushroom/mushroom';
 
-// import utils from '../../helpers/utils';
+const removeShroomEvent = (e) => {
+  const mushroomId = e.target.closest('.card').id;
+
+  mushroomData.deleteMushroom(mushroomId)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      buildForest();
+    })
+    .catch((err) => console.error('could not delete mushroom', err));
+};
 
 const buildForest = () => {
   mushroomData.getMushrooms()
@@ -18,6 +27,7 @@ const buildForest = () => {
       domString += '</div>';
 
       utils.printToDom('#forest', domString);
+      $('body').on('click', '.delete-shroom', removeShroomEvent);
     })
     .catch((err) => console.error('got mushrooms broke', err));
 };
