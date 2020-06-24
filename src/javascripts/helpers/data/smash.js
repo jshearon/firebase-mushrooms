88d253ac51a6ca704df3a7528b14fe014bbf14da
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-06-23 20:19:11 -0500

    added mushrooms owned

---
+++ src/javascripts/helpers/data/smash.js
@@ -0,0 +1,24 @@
+import mushroomData from './mushroomData';
+import mycologistData from './mycologistData';
+import mycologistMushroomData from './mycologistMushroomData';
+
+const getSingleMycoWithShrooms = (mycologistId) => new Promise((resolve, reject) => {
+  mycologistData.getMycologistById(mycologistId)
+    .then((response) => {
+      const mycologist = response.data;
+      mycologist.id = mycologistId;
+      mycologist.mushrooms = [];
+      mycologistMushroomData.getMycoShroomsByMycoUid(mycologist.uid).then((mycoShrooms) => {
+        mushroomData.getMushrooms().then((allMushrooms) => {
+          mycoShrooms.forEach((mycoShroom) => {
+            const mushroom = allMushrooms.find((m) => m.id === mycoShroom.mushroomId);
+            mycologist.mushrooms.push(mushroom);
+          });
+          resolve(mycologist);
+        });
+      });
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getSingleMycoWithShrooms };
